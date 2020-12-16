92d6baef79c153562dd2524e96d442454358d5f4
felipeplataa <71336009+felipeplataa@users.noreply.github.com>
2020-12-15 19:48:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ origins = [
     "http://localhost.tiangolo.com",
     "http://localhost", 
     "http://localhost:8080",
-    "https://g3m4-g10-catalogo-app.herokuapp.com/"
+    "https://g3m4-g10-catalogo-app.herokuapp.com"
 ]
 
 api.add_middleware(
